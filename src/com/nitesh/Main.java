ee8d9e4ae2d8386f6251923153044879e7316730
Nitesh Ramola <nitesh.ramola@shippigo.com>
2023-04-29 20:05:38 +0530

    taking user input

--- src/com/nitesh/Main.java
+++ src/com/nitesh/Main.java
@@ -1,14 +1,12 @@
 package com.nitesh;
 
-import java.awt.*;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Point point = new Point(1, 2);
-        Point point1 = point;
-
-        point1.x = 5;
-
-        System.out.println(point1);
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Age: ");
+        final String age = scanner.nextLine();
+        System.out.println("Your age is: " + age);
     }
 }
