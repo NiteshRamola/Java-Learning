4f0dee5877ca6adbf6b8fda5e59643f63028432a
Nitesh Ramola <nitesh.ramola@shippigo.com>
2023-04-29 18:46:01 +0530

    reference type

--- src/com/nitesh/Main.java
+++ src/com/nitesh/Main.java
@@ -1,8 +1,14 @@
 package com.nitesh;
 
+import java.awt.*;
+
 public class Main {
     public static void main(String[] args) {
+        Point point = new Point(1, 2);
+        Point point1 = point;
 
-        System.out.println("Hello world!");
+        point1.x = 5;
+
+        System.out.println(point1);
     }
 }
